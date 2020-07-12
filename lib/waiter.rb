ef9737112324a69f5d9f8b4a2d0166cea6764ed0
kowalix007 L.K <kowalix007@gmail.com>
2020-07-12 14:56:50 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,26 @@
 class Waiter
+  attr_reader :name
+  attr_accessor :yrs_experience
+
+ @@all = []
+
+  def initialize (name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer,total, tip)
+    Meal.new(self,customer,total,tip)
+  end
+
+
+
+
+
 
 end
